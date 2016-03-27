dd6c17754607e40a8ebd28ce9e9dae45689d8775
Nina Rabinowitz <rabinowitz.nina@gmail.com>
2016-03-27 17:51:38 +0300

    Update Capstone Code.R

--- Capstone Code.R
+++ Capstone Code.R
@@ -5,7 +5,7 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 library(gtools)
-dataFolder <- "C:/Users/Nina/Desktop/datafoundations/Capstone Project/"
+dataFolder <- "<your file path here>"
 
 #load all the data gathered related to my capstone project
 allprices <- read_excel(paste0(dataFolder, "all_prices.xlsx"))
